4daf8d57533d52faffd81ca609bc3b372f8511df
Donna Amos <donnacamos.developer@gmail.com>
2019-12-18 02:06:32 +0000

    Automatically backed up by Learn

---
+++ src/createStore.js
@@ -0,0 +1,18 @@
+export default function createStore(reducer) {
+  let state 
+
+  function getState() {
+    return state 
+  }
+
+  function dispatch(action){
+    state = reducer(state, action)
+  }
+
+  dispatch({type: "@@INIT"}) 
+
+  return {
+    getState, 
+    
+  }
+}
