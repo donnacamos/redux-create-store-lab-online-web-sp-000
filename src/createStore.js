8339d028b30e427214d3b8e9b942d65425fa9f02
Donna Amos <donnacamos.developer@gmail.com>
2019-12-18 02:10:02 +0000

    Automatically backed up by Learn

--- src/createStore.js
+++ src/createStore.js
@@ -13,6 +13,10 @@ export default function createStore(reducer) {
 
   return {
     getState,
-
+    dispatch
   }
+}
+
+function render() {
+  const container = document.getElementById('container');
 }
