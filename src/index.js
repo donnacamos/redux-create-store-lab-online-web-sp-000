eff75f1ceba03e10396d7c67b65095bad4a2cc2b
Donna Amos <donnacamos.developer@gmail.com>
2019-12-18 02:11:38 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,27 @@
 // write your createStore function here
+export default function createStore(reducer) {
+  let state
+
+  function getState() {
+    return state
+  }
+
+  function dispatch(action){
+    state = reducer(state, action)
+  }
+
+  dispatch({type: "@@INIT"})
+
+  return {
+    getState,
+    dispatch
+  }
+}
+
+function render() {
+  const container = document.getElementById('container');
+}
+
 
 function candyReducer(state = [], action) {
   switch (action.type) {
